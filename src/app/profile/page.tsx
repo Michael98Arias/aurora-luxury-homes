6caeaab659f3063a00c12636159054de90893e32
Michael98Arias <maicol98arias@gmail.com>
2025-09-15 05:36:52 -0500

    feature: Add login and profile view layouts and structure

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import ProfileForm from "@/components/ProfileForm/ProfileForm";
+
+const ProfilePage = () => {
+  return (
+    <div className="profile-page">
+      <ProfileForm />
+    </div>
+  );
+};
+
+export default ProfilePage;
